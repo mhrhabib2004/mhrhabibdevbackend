a1308fdfb7fba3da48e72100ea6fa41d11175c04
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-03-08 02:54:06 +0600

    up:blog interface

--- src/app/modules/blog/blog.interface.ts
+++ src/app/modules/blog/blog.interface.ts
@@ -1,10 +1,11 @@
 
-import { Types } from "mongoose";
 
 export type Tblog = {
     _id?: string,
     title: string;
     content: string;
-    author: Types.ObjectId;
-    isPublished: boolean;
+    image?: string;
+    category?: string;
 };
+
+
