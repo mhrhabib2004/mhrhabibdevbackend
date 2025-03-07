3b75d6d9bc8324bc592cb1b91aaeace1c9801ea7
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-03-08 02:53:45 +0600

    up:blog controller

--- src/app/modules/blog/blog.controller.ts
+++ src/app/modules/blog/blog.controller.ts
@@ -1,15 +1,13 @@
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { blogService } from "./blog.service";
-import httpStatus from 'http-status';
+import httpStatus from "http-status";
 
 
 // Create Blogs
 const createblogs = catchAsync(async (req, res) => {
-    
-    const tokenId = req.user.userId
-    
-    const result = await blogService.createBlogIntoDB(req.body, tokenId);
+
+    const result = await blogService.createBlogIntoDB(req.body);
 
     sendResponse(res, {
         statusCode: httpStatus.CREATED,
@@ -22,10 +20,9 @@ const createblogs = catchAsync(async (req, res) => {
 // All Blog data
 const getAllBlog = catchAsync(async (req, res) => {
 
-    const query = req.query;
-    
-    const result = await blogService.getAllBlogFromDB(query);
-    
+
+    const result = await blogService.getAllBlogFromDB();
+
     sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
@@ -34,29 +31,26 @@ const getAllBlog = catchAsync(async (req, res) => {
     });
 });
 
-// Single Blog data get
-// const getSingleBlog = catchAsync(async (req, res) => {
-//     const { blogId: id } = req.params;
-//     const payload = {};
-//     const result = await blogService.getSingleBlogFromDB( id, payload );
 
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: ' Single Blog data get succesfully',
-//         data: result,
-//     });
-// });
+// Get Single Project
+const getSingleBlog = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await blogService.getSingleBlogFromDB(id);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Single Blog is retrieved succesfully',
+        data: result,
+    });
+});
+
 
 // Update Blog
 const updateBlog = catchAsync(async (req, res) => {
     // console.log('test', req.user);
     const { id } = req.params;
-    const result =
-        await blogService.updateBlogIntoDB(
-            id,
-            req.body,
-        );
+    const result = await blogService.updateBlogIntoDB(id, req.body);
 
     sendResponse(res, {
         statusCode: httpStatus.OK,
@@ -85,8 +79,8 @@ const deleteBlog = catchAsync(async (req, res) => {
 export const BlogsControllers = {
     createblogs,
     getAllBlog,
-    // getSingleBlog,
     updateBlog,
-    deleteBlog
+    deleteBlog,
+    getSingleBlog
 
 };
