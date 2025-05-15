988e64fa1990232250824121cb535230d3ada0d5
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-05-15 23:14:53 +0600

    up:project interface

--- src/app/modules/projects/project.interface.ts
+++ src/app/modules/projects/project.interface.ts
@@ -1,11 +1,20 @@
 
 
 export type TProject = {
-    _id?: string;
-    title: string;
-    descriptions: string;
-    liveLink: string;
-    image?: string;
-    githubClient?: string
-    githubServer?: string
+  _id?: string;
+  title: string;
+  descriptions: string;
+  liveLink: string;
+  image?: string;
+  githubClient?: string;
+  githubServer?: string;
+  techStack?: string[];
+  features?: string[];
+  createdAt?: string;
+  updatedAt?: string;
+  category?: string;
+  isTeamProject?: boolean;
+  contributors?: string[];
+  videoDemo?: string;
+
 };
